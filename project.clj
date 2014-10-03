d6fd4fcd0388cc1bdeb4278a09e0c000dec1b7da
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2014-10-02 18:16:28 -0700

    Prepare for next release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jafingerhut/dolly "0.1.0"
+(defproject jafingerhut/dolly "0.2.0-SNAPSHOT"
   :description "Clojure namespace cloning"
   :url "http://github.com/jafingerhut/dolly"
   :license {:name "Eclipse Public License"
